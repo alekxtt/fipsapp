6535a2970d034d608fdf8e9a61b30087b7e35db8
Александр Тутушкин <alekxtt@mail.ru>
2021-11-11 12:33:16 +0400

    new commit

--- fipsapp/settings.py
+++ fipsapp/settings.py
@@ -68,17 +68,22 @@ WSGI_APPLICATION = 'fipsapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'fipsapp', 
+#         'USER': 'postgres', 
+#         'PASSWORD': 'Sander66_33',
+#         'HOST': '127.0.0.1', 
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'fipsapp', 
-        'USER': 'postgres', 
-        'PASSWORD': 'Sander66_33',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
